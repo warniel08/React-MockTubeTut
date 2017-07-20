f028e00a3080235240662b2397a8db9db425ad1c
Warner Nielsen <bwn888@gmail.com>
2017-07-19 23:07:25 -0600

    Update search bar to have state and to render user input

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 // Create a new component. This component should produce some HTML
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML and put it on the page(in the DOM)
